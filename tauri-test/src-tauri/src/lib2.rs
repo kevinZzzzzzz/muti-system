6797cc5522cb684c316055cfc7c0b031c3ec9ee3
Zhang YingJie <jie736805161@qq.com>
2025-03-26 14:29:33 +0800

    feat: 调用打印机

--- tauri-test/src-tauri/src/lib2.rs
+++ tauri-test/src-tauri/src/lib2.rs
@@ -1,36 +1,24 @@
-use printers::{get_printers};
+use printers::{get_default_printer, get_printers};
+use serde::Serialize;
 
-// use serde::Serialize;
-use printers;
-
-// #[derive(Serialize)]
-// struct PrinterInfo {
-//     name: String,
-//     is_default: bool,
-//     state: String,
-//     driver: Option<String>
-// }
-
+#[derive(Serialize)]
+pub struct PrinterInfo {
+    name: String,
+    driver: Option<String>,
+    state: Option<String>,
+    is_default: bool,
+}
 
 #[tauri::command]
-pub fn get_printer_list() {
-  let printers = get_printers();
-  // let default_name = get_default_printer();
-  // for printer in printers {
-  
-  //     print!("{:?}", printer);
-  // }
-  println!("{:?}", printers);
-  // printers.map(|printer| {
-  //   Ok(PrinterInfo {
-  //       name: printer.name().to_string(),
-  //       is_default: printer.name() == default_name,
-  //       state: match printer.state() {
-  //           Some(state) => format!("{:?}", state), // 将枚举转为字符串
-  //           None => "未知状态".into()
-  //       },
-  //       driver: printer.driver().map(|s| s.to_string())
-  //   })
-  // })
-  // .collect()
+pub fn get_printer_list() -> Vec<PrinterInfo> {
+    let default_printer = get_default_printer();
+    get_printers()
+        .into_iter()
+        .map(|printer| PrinterInfo {
+            name: printer.name.clone(),
+            driver: Some(printer.driver_name.clone()),
+            state: Some(format!("{:?}", printer.state)),
+            is_default: default_printer.as_ref().map_or(false, |default| printer.name == default.name),
+        })
+        .collect()
 }
